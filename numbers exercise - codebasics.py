7336a7fe06719c3913109bb46773bc37991013ee
lory!! <61609681+loryvi@users.noreply.github.com>
2021-08-17 15:17:17 +0800

    Create numbers exercise - codebasics.py
    my answer on codebasics exercise on numbers

---
+++ numbers exercise - codebasics.py
@@ -0,0 +1,19 @@
+length = 92
+width = 48.8
+area = length * width
+
+print("The area of football field is {:.2f}".format(area))
+
+potatochips = 9
+price_potatochips = 1.49
+pay = 20
+change = 20 - (potatochips * price_potatochips)
+print(f"Your change is:", change)
+
+bathroom_size = 5.5**2
+tiles_price = 500
+cost = bathroom_size * tiles_price
+print("The cost is $",cost)
+
+num=17
+print('Binary of number 17 is:',format(num,'b'))
